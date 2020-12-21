c8c373e0a1d752515d352d011581e2e5f4389053
Den <47720553+HeartR8@users.noreply.github.com>
2020-12-21 15:35:01 +0300

    Added redux

--- spotify-api-ui/src/index.js
+++ spotify-api-ui/src/index.js
@@ -6,15 +6,19 @@ import SpotifyApp from './app/main/SpotifyApp';
 import Authorization from "./app/main/Authorization";
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './app/redux/store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <Switch>
-                <Route exact path={"/login"} component={Authorization}/>
-                <Route exact path={"/"} component={SpotifyApp}/>
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path={"/login"} component={Authorization}/>
+                    <Route exact path={"/"} component={SpotifyApp}/>
+                </Switch>
+            </Router>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
