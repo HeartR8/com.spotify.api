8eab4f5b982b594ac4a395a1a5e83abde3ca526c
Nikita <kononenko.n@phystech.edu>
2020-12-21 15:35:09 +0300

    added news

--- spotify-api-ui/src/index.js
+++ spotify-api-ui/src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import SpotifyApp from './app/main/SpotifyApp';
 import Authorization from "./app/main/Authorization";
+import NewsPage from "./app/main/NewsPage";
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
@@ -11,6 +12,7 @@ ReactDOM.render(
     <React.StrictMode>
         <Router>
             <Switch>
+                <Route exact path={"/news"} component={NewsPage}/>
                 <Route exact path={"/login"} component={Authorization}/>
                 <Route exact path={"/"} component={SpotifyApp}/>
             </Switch>
